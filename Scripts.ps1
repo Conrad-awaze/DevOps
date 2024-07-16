17caa8f289aabfad0902c2a38873e0402163d43d
Conrad Gauntlett <conrad.gauntlett@awaze.com>
2024-07-16 21:38:56 +0100

    updates

--- Scripts.ps1
+++ Scripts.ps1
@@ -1,4 +1,4 @@
-Remove-Module DevOpsToolkit
+# Remove-Module DevOpsToolkit
 Import-Module AWS.Tools.Common, AWS.Tools.DynamoDBv2, AWS.Tools.RDS
 Import-Module '/Users/conrad.gauntlett/WorkArea/Repos/DBA_MISC/PowerShell/Modules/DevOpsToolkit'
 
@@ -17,98 +17,151 @@ $Summary | Format-Table -AutoSize
 
 #endregion
 
-# # -------------------------------------------------------------------------------------------------------------------------------------------------- #
-# #                                                                   DYNAMODB TABLES                                                                  #
-# # -------------------------------------------------------------------------------------------------------------------------------------------------- #
-# #region DynamoDB Tables
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+#                                                                   DYNAMODB TABLES                                                                  #
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+#region DynamoDB Tables
 
-# $ddbTables = Get-DoDDBTableInformation $Profiles $Regions 
-# $ddbTables | Format-Table -AutoSize
+$ddbTables = Get-DoDDBTableInformation $Profiles $Regions 
+$ddbTables | Format-Table -AutoSize
 
-# #endregion
+#endregion
 
-# # -------------------------------------------------------------------------------------------------------------------------------------------------- #
-# #                                                                LIST DYNAMODB BACKUPS                                                               #
-# # -------------------------------------------------------------------------------------------------------------------------------------------------- #
-# #region DynamoDB Backups
+# $Region = 'eu-central-1'
 
-# $BackupsList    = Get-DDBBackupList -ProfileName $Profiles.ProfileName -Region eu-west-2
-# $Columns        = 'TableName', 'BackupName', 'BackupCreationDateTime',    'BackupType' #  'BackupStatus','BackupSizeBytes',
-# $BackupsList | Sort-Object BackupCreationDateTime -Descending | Select-Object -Property $Columns | Format-Table -AutoSize
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+#                                                                LIST DYNAMODB BACKUPS                                                               #
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+#region DynamoDB Backups
 
-# #endregion
+$BackupsList = @()
+$RegexRegion = '(?<region>\w+-\w+-\d+)'
+$Regions | ForEach-Object {
 
-# # -------------------------------------------------------------------------------------------------------------------------------------------------- #
-# #                                                          GET THE LATEST BACKUP FOR A TABLE                                                         #
-# # -------------------------------------------------------------------------------------------------------------------------------------------------- #
-# #region Get the latest backup for a table
+    $BkList    = Get-DDBBackupList -ProfileName $Profiles.ProfileName -Region $_
+    
+    $BkList | ForEach-Object {
 
-# $Table = 'terraform-locks'
+        $Bk = [PSCustomObject]@{
 
-# if($BackupsList){
+            Region = (($_.BackupArn | Select-String -Pattern $RegexRegion).Matches.Groups | Where-Object { $_.Name -eq 'region' }).Value
+            BackupArn = $_.BackupArn
+            BackupCreationDateTime = $_.BackupCreationDateTime
+            BackupName = $_.BackupName
+            BackupSizeBytes = $_.BackupSizeBytes
+            BackupStatus = $_.BackupStatus
+            BackupType = $_.BackupType
+            TableArn = $_.TableArn
+            TableId = $_.TableId
+            TableName = $_.TableName
+        }
+        $BackupsList += $Bk
+    }
+}
 
-#     $Backup = $BackupsList | Where-Object { $_.TableName -eq $Table } | Sort-Object -Property BackupCreationDateTime -Descending | Select-Object -First 1
-#     $Bk = Get-DDBBackup -BackupArn $Backup.BackupArn -ProfileName $Profiles.ProfileName
-#     $Bk.BackupDetails
+$Columns        = 'Region', 'TableName', 'BackupName', 'BackupCreationDateTime',    'BackupType' #  'BackupStatus','BackupSizeBytes',
+$BackupsList | Sort-Object BackupCreationDateTime -Descending | Select-Object -Property $Columns | Format-Table -AutoSize
 
-# }
+#endregion
 
-# #endregion
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+#                                                          GET THE LATEST BACKUP FOR A TABLE                                                         #
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+#region Get the latest backup for a table
 
-# # -------------------------------------------------------------------------------------------------------------------------------------------------- #
-# #                                                          CREATE A NEW BACKUP FOR THE TABLE                                                         #
-# # -------------------------------------------------------------------------------------------------------------------------------------------------- #
-# #region Create a new backup for the table
+# $RegexRegion = '(?<region>\w+-\w+-\d+)'
+# $Region = (($BackupsList.BackupArn | Select-String -Pattern $RegexRegion).Matches.Groups | Where-Object { $_.Name -eq 'region' }).Value
 
-# $Table = 'terraform-locks'
-# $BackupNew = New-DDBBackup -TableName $Table -ProfileName $Profiles.ProfileName -BackupName "$Table-$(get-date -format "yyyy-MM-dd-HH-mm-ss")" -Region eu-west-2
-# $BackupNew
+$Table = 'terraform-locks'
 
-# #endregion
+if($BackupsList){
+
+    $Backup = $BackupsList | Where-Object { $_.TableName -eq $Table } | Sort-Object -Property BackupCreationDateTime -Descending | Select-Object -First 1
+    $Bk = Get-DDBBackup -BackupArn $Backup.BackupArn -ProfileName $Profiles.ProfileName -Region $Backup.Region
+    $Bk.BackupDetails
+
+}
+
+#endregion
+
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+#                                                          CREATE A NEW BACKUP FOR THE TABLE                                                         #
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+#region Create a new backup for the table
+
+$Table = 'terraform-locks'
+$BackupNew = New-DDBBackup -TableName $Table -ProfileName $Profiles.ProfileName -BackupName "$Table-$(get-date -format "yyyy-MM-dd-HH-mm-ss")" -Region eu-central-1
+$BackupNew
+
+#endregion
 
 
-# # -------------------------------------------------------------------------------------------------------------------------------------------------- #
-# #                                                              CHECK NEW BACKUP DETAILS                                                              #
-# # -------------------------------------------------------------------------------------------------------------------------------------------------- #
-# #region Check the new backup details
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+#                                                              CHECK NEW BACKUP DETAILS                                                              #
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+#region Check the new backup details
 
-# $Bk = Get-DDBBackup -BackupArn $BackupNew.BackupArn -ProfileName $Profiles.ProfileName -Region eu-west-2
-# $Bk.BackupDetails
+$Bk = Get-DDBBackup -BackupArn $BackupNew.BackupArn -ProfileName $Profiles.ProfileName -Region eu-central-1
+$Bk.BackupDetails
 
-# #endregion
+#endregion
 
-# # -------------------------------------------------------------------------------------------------------------------------------------------------- #
-# #                                                                  LIST ALL BACKUPS                                                                  #
-# # -------------------------------------------------------------------------------------------------------------------------------------------------- #
-# #region List all backups
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+#                                                                  LIST ALL BACKUPS                                                                  #
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+#region List all backups
 
-# $BackupsList    = Get-DDBBackupList -ProfileName $Profiles.ProfileName -Region eu-west-2
-# $Columns        = 'TableName', 'BackupName', 'BackupCreationDateTime',    'BackupType' #  'BackupStatus','BackupSizeBytes',
-# $BackupsList | Sort-Object BackupCreationDateTime -Descending | Select-Object -Property $Columns | Format-Table -AutoSize
+$BackupsList = @()
+$RegexRegion = '(?<region>\w+-\w+-\d+)'
+$Regions | ForEach-Object {
 
-# #endregion
+    $BkList    = Get-DDBBackupList -ProfileName $Profiles.ProfileName -Region $_
+    
+    $BkList | ForEach-Object {
 
-# # -------------------------------------------------------------------------------------------------------------------------------------------------- #
-# #                                                                GET THE OLDEST BACKUP                                                               #
-# # -------------------------------------------------------------------------------------------------------------------------------------------------- #
-# #region Get the oldest backup
+        $Bk = [PSCustomObject]@{
 
-# $Table = 'terraform-locks'
-# $BackupOLDEST = $BackupsList | Where-Object { $_.TableName -eq $Table } | Sort-Object -Property BackupCreationDateTime  | Select-Object -First 1
-# $Bk = Get-DDBBackup -BackupArn $BackupOLDEST.BackupArn -ProfileName $Profiles.ProfileName
-# $Bk.BackupDetails
+            Region = (($_.BackupArn | Select-String -Pattern $RegexRegion).Matches.Groups | Where-Object { $_.Name -eq 'region' }).Value
+            BackupArn = $_.BackupArn
+            BackupCreationDateTime = $_.BackupCreationDateTime
+            BackupName = $_.BackupName
+            BackupSizeBytes = $_.BackupSizeBytes
+            BackupStatus = $_.BackupStatus
+            BackupType = $_.BackupType
+            TableArn = $_.TableArn
+            TableId = $_.TableId
+            TableName = $_.TableName
+        }
+        $BackupsList += $Bk
+    }
+}
 
-# #endregion
+$Columns        = 'Region', 'TableName', 'BackupName', 'BackupCreationDateTime',    'BackupType' #  'BackupStatus','BackupSizeBytes',
+$BackupsList | Sort-Object BackupCreationDateTime -Descending | Select-Object -Property $Columns | Format-Table -AutoSize
 
-# # -------------------------------------------------------------------------------------------------------------------------------------------------- #
-# #                                                              DELETE THE OLDEST BACKUP                                                              #
-# # -------------------------------------------------------------------------------------------------------------------------------------------------- #
-# #region Delete the oldest backup
+#endregion
 
-# $BackupDelete = Remove-DDBBackup -BackupArn $BackupOLDEST.BackupArn -ProfileName $Profiles.ProfileName -Region eu-west-2 -Confirm:$false
-# $BackupDelete.BackupDetails
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+#                                                                GET THE OLDEST BACKUP                                                               #
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+#region Get the oldest backup
 
-# #endregion
+$Table = 'terraform-locks'
+$BackupOLDEST = $BackupsList | Where-Object { $_.TableName -eq $Table } | Sort-Object -Property BackupCreationDateTime  | Select-Object -First 1
+$Bk = Get-DDBBackup -BackupArn $BackupOLDEST.BackupArn -ProfileName $Profiles.ProfileName -Region $BackupOLDEST.Region
+$Bk.BackupDetails
+
+#endregion
+
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+#                                                              DELETE THE OLDEST BACKUP                                                              #
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+#region Delete the oldest backup
+
+$BackupDelete = Remove-DDBBackup -BackupArn $BackupOLDEST.BackupArn -ProfileName $Profiles.ProfileName -Region $BackupOLDEST.Region -Confirm:$false
+$BackupDelete.BackupDetails
+
+#endregion
 
 
 # -------------------------------------------------------------------------------------------------------------------------------------------------- #
@@ -122,7 +175,14 @@ $RDS | Format-Table -AutoSize
 
 #endregion
 
-# $DB = ($RDS | Where-Object { $_.Snapshots -gt 0 } | Select-Object -First 1).DBInstanceIdentifier
-$DB  = 'drupal-db-prod'
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+#                                                                    RDS SNAPSHOTS                                                                   #
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+#region RDS Snapshots
+
+$DB  = 'novasol-db-prod'
 $Snapshots | Where-Object {$_.DBInstanceIdentifier -eq $DB} | Select-Object $RDSSummaryColumns |
 Sort-Object SnapshotCreateTime -Descending  |Format-Table -AutoSize
+
+#endregion
+
